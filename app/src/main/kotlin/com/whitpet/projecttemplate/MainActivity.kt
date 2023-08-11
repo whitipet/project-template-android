88f91743db6db3ad9dae9c4d5ba5d77619fba077
whitipet <whitipet@gmail.com>
2023-08-11 22:00:24 +0300

    Init commit

---
+++ app/src/main/kotlin/com/whitpet/projecttemplate/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.whitpet.projecttemplate
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(View(this))
+    }
+}
