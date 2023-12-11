e7c10815dd656a80b9e1414faa8164e6c646c832
whitipet <whitipet@gmail.com>
2023-12-11 07:17:11 +0200

    Refactor :platform:stdlib-android View

--- platform/stdlib-android/src/main/kotlin/platform/stdlib_android/View.kt
+++ platform/stdlib-android/src/main/kotlin/platform/stdlib_android/View.kt
@@ -3,12 +3,11 @@ package platform.stdlib_android
 import android.view.View
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
 
-fun View.retrieveSize(requireNonZeroSides: Boolean = false, action: (width: Int, height: Int) -> Unit) {
+fun View.retrieveSize(waitNonZeroSides: Boolean = false, action: (width: Int, height: Int) -> Unit) {
 	viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
 		override fun onGlobalLayout() {
-			if (requireNonZeroSides) {
-				if (width != 0 && height != 0) viewTreeObserver.removeOnGlobalLayoutListener(this)
-			} else viewTreeObserver.removeOnGlobalLayoutListener(this)
+			if (waitNonZeroSides) if (width > 0 && height > 0) viewTreeObserver.removeOnGlobalLayoutListener(this)
+			else viewTreeObserver.removeOnGlobalLayoutListener(this)
 			action.invoke(width, height)
 		}
 	})
