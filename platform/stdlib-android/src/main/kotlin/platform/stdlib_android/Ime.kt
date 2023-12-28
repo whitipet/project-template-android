bd3fc2768a4c7cfde6b115f0d89b42132a0e1cd3
whitipet <whitipet@gmail.com>
2023-12-28 20:58:12 +0200

    Refactor :platform:stdlib_android Ime

--- platform/stdlib-android/src/main/kotlin/platform/stdlib_android/Ime.kt
+++ platform/stdlib-android/src/main/kotlin/platform/stdlib_android/Ime.kt
@@ -1,19 +1,32 @@
 package platform.stdlib_android
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
 import android.view.View
+import android.view.Window
 import android.view.inputmethod.InputMethodManager
 import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat.Type.ime
 
 fun View.showIme(requestFocus: Boolean = false) {
 	if (requestFocus) requestFocus()
-	ViewCompat.getWindowInsetsController(this)?.show(WindowInsetsCompat.Type.ime())
+	getWindows()?.let { w -> WindowCompat.getInsetsController(w, this).show(ime()) }
 }
 
 fun View.hideIme(clearFocus: Boolean = false) {
 	if (clearFocus) clearFocus()
-	ViewCompat.getWindowInsetsController(this)?.hide(WindowInsetsCompat.Type.ime())
+	getWindows()?.let { w -> WindowCompat.getInsetsController(w, this).hide(ime()) }
+}
+
+private fun View.getWindows(): Window? {
+	var c: Context = context
+	while (c is ContextWrapper) {
+		if (c is Activity) return c.window
+		c = c.baseContext
+	}
+	return null
 }
 
 fun View.restartInput() = getSystemService(context, InputMethodManager::class.java)?.restartInput(this)
